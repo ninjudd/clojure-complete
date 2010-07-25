5dd56428d2b0ca1e99bacaec389681d50c696c8e
Justin Balthrop <git@justinbalthrop.com>
2010-07-25 11:04:59 -0700

    add clojure-useful dep

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject clojure-complete "0.1.0"
+(defproject clojure-complete "0.1.1"
   :description "Standalone completion library adapted from swank-clojure"
-  :dependencies [[clojure "1.2.0-master-SNAPSHOT"]])
+  :dependencies [[clojure "1.2.0-beta1"]
+                 [clojure-useful "0.2.4"]])
