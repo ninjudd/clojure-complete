d39471d98d0d69f6ec14a3f517774343b9b26004
Justin Balthrop <git@justinbalthrop.com>
2010-07-28 11:20:52 -0700

    update useful

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clojure-complete "0.1.1"
   :description "Standalone completion library adapted from swank-clojure"
   :dependencies [[clojure "1.2.0-beta1"]
-                 [clojure-useful "0.2.4"]])
+                 [clojure-useful "0.2.5"]])
