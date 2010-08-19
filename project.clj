08c1cc9cd82ef753e003a54a6f69592aa490d96c
Justin Balthrop <git@justinbalthrop.com>
2010-08-19 08:43:26 -0700

    update to clojure 1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clojure-complete "0.1.2"
   :description "Standalone completion library adapted from swank-clojure"
-  :dependencies [[clojure "1.2.0-beta1"]
+  :dependencies [[clojure "1.2.0"]
                  [clojure-useful "0.2.5"]])
