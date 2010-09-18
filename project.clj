9ff9e878e58da7f7e01d0a8768d6eba743dbbb0f
ninjudd <git@justinbalthrop.com>
2010-09-17 18:05:29 -0700

    remove dependency on useful

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject clojure-complete "0.1.3"
   :description "Standalone completion library adapted from swank-clojure"
-  :dependencies [[clojure "1.2.0"]
-                 [clojure-useful "0.2.6"]])
+  :dependencies [[clojure "1.2.0"]])
