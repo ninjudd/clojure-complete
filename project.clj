caaf82fbbe840f6e3416b029344f27e1e72a13ca
Justin Balthrop <git@justinbalthrop.com>
2012-01-27 14:54:24 -0800

    0.2.0

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject clojure-complete "0.1.4"
+(defproject clojure-complete "0.2.0"
   :description "Standalone completion library adapted from swank-clojure"
   :dependencies [[clojure "1.3.0"]])
