2d040fd28c56c413d4b601f793a9e3e0023b822f
Colin Jones <trptcolin@gmail.com>
2012-01-29 13:49:08 -0600

    Unwrap exception to get the bottom-most cause
    Also add a test and make sure it fails before the fix.

--- test/complete/core_test.clj
+++ test/complete/core_test.clj
@@ -2,11 +2,14 @@
   (:use complete.core clojure.test))
 
 (deftest completions-test
-  (is '("alength" "alias" "all-ns" "alter" "alter-meta!" "alter-var-root")
-      (completions "al" 'clojure.core))
+  (is (= '("alength" "alias" "all-ns" "alter" "alter-meta!" "alter-var-root")
+         (completions "al" 'clojure.core)))
 
-  (is '("jio/make-input-stream" "jio/make-output-stream" "jio/make-parents" "jio/make-reader" "jio/make-writer")
-      (completions "jio/make" 'clojure.core))
+  (is (= '("jio/make-input-stream" "jio/make-output-stream" "jio/make-parents" "jio/make-reader" "jio/make-writer")
+         (completions "jio/make" 'clojure.core)))
 
-  (is '("complete.core" "complete.core-test")
-      (completions "complete.core")))
+  (is (= '("clojure.core/alter" "clojure.core/alter-meta!" "clojure.core/alter-var-root")
+         (completions "clojure.core/alt" 'clojure.core)))
+
+  (is (= '("complete.core" "complete.core-test")
+         (completions "complete.core"))))
