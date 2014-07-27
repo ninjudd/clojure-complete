e37c5b0a3c05d1b6481c60f42e9979a784d3f7c4
Colin Jones <trptcolin@gmail.com>
2014-07-27 10:48:20 -0500

    Move to snapshot for next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-complete "0.2.4"
+(defproject clojure-complete "0.2.5-SNAPSHOT"
   :description "Standalone completion library adapted from swank-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
