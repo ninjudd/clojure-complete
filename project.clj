62e60b1881a8cd5623578dac35f40dd99779f8be
Colin Jones <trptcolin@gmail.com>
2014-07-27 10:46:04 -0500

    Bump to 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-complete "0.2.3"
+(defproject clojure-complete "0.2.4"
   :description "Standalone completion library adapted from swank-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
